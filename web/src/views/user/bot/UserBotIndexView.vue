d5b42966c2fe42a4cb1fac61f9131fad8db2a8b2
zihui <3335914015@qq.com>
2023-06-18 15:14:32 +0800

    修复了bot getlist 看不到的问题

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -146,7 +146,7 @@ export default {
 
         const refresh_bots = () => {
             $.ajax({
-                url: "http://127.0.0.1:3000/kob/user//bot/getlist/",
+                url: "http://127.0.0.1:3000/kob/user/bot/getlist/",
                 type: "get",
                 headers: {
                     Authorization: "Bearer " + store.state.user.token,
